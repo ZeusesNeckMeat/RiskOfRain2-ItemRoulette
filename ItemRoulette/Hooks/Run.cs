0bcb81a4a9c874013d261328b1c7035f9947e6cb
ZeusesNeckMeat <gruber2552@hotmail.com>
2022-01-15 00:32:25 -0500

    Added config for Guaranteed Items, and other things

---
+++ ItemRoulette/Hooks/Run.cs
@@ -0,0 +1,122 @@
+﻿using BepInEx.Logging;
+using On.RoR2.Artifacts;
+
+using Hook = On.RoR2;
+using RoR2Run = RoR2.Run;
+
+namespace ItemRoulette.Hooks
+{
+    internal class Run
+    {
+        private readonly ManualLogSource _logger;
+        private readonly ConfigSettings _configSettings;
+        private readonly CustomDropTable _customDropTable;
+
+        private bool _isBuildDropTableDone = false;
+        private bool _isMonsterGenerateAvailableItemsSetDone = false;
+        private bool _isItemCatalogInitDone = false;
+        private bool _isPickupCatalogInitDone = false;
+
+        private int _currentLoopCount = 0;
+
+        public Run(ManualLogSource logger, ConfigSettings configSettings, CustomDropTable customDropTable)
+        {
+            _logger = logger;
+            _configSettings = configSettings;
+            _customDropTable = customDropTable;
+        }
+
+        public void OnBuildDropTable(Hook.Run.orig_BuildDropTable orig, RoR2Run self)
+        {
+            //clear default loaded lists first
+            _logger.LogInfo("Building full drop table before changing items");
+            orig(self);
+
+            _isBuildDropTableDone = true;
+
+            if (_isBuildDropTableDone && _isMonsterGenerateAvailableItemsSetDone)
+                _customDropTable.BuildDropTable();
+        }
+
+        public void OnBeginStage(Hook.Run.orig_BeginStage orig, RoR2Run self)
+        {
+            orig(self);
+
+            if (RoR2Run.instance.stageClearCount == 0)
+                return;
+
+            var newLoopStarted = RoR2Run.instance.loopClearCount > _currentLoopCount;
+            if (newLoopStarted)
+                _currentLoopCount = RoR2Run.instance.loopClearCount;
+
+            var shouldRefreshItemPool = _configSettings.ItemRefreshOptions == ItemRefreshOptions.EachStage
+                                        || (_configSettings.ItemRefreshOptions == ItemRefreshOptions.EachLoop && newLoopStarted);
+
+            if (!shouldRefreshItemPool)
+                return;
+
+            _customDropTable.ResetInstanceDropLists();
+            _customDropTable.BuildDropTable();
+        }
+
+        public void OnRunDestroy(Hook.Run.orig_OnDestroy orig, RoR2Run self)
+        {
+            _logger.LogInfo("Run Over. Cleaning up.");
+            _isItemCatalogInitDone = false;
+            _isPickupCatalogInitDone = false;
+            _isMonsterGenerateAvailableItemsSetDone = false;
+            _isBuildDropTableDone = false;
+            _currentLoopCount = 0;
+
+            _configSettings.RefreshConfigSettings();
+
+            orig(self);
+        }
+
+        public void OnGenerateAvailableItemsSet(MonsterTeamGainsItemsArtifactManager.orig_GenerateAvailableItemsSet orig)
+        {
+            orig();
+            _isMonsterGenerateAvailableItemsSetDone = true;
+
+            if (_isBuildDropTableDone && _isMonsterGenerateAvailableItemsSetDone)
+                _customDropTable.BuildDropTable();
+        }
+
+        public void OnItemCatalogInit(Hook.ItemCatalog.orig_Init orig)
+        {
+            orig();
+            _isItemCatalogInitDone = true;
+
+            if (_isItemCatalogInitDone && _isPickupCatalogInitDone)
+                GenerateItemLists();
+        }
+
+        public void OnPickupCatalogInit(Hook.PickupCatalog.orig_Init orig)
+        {
+            orig();
+            _isPickupCatalogInitDone = true;
+
+            if (_isItemCatalogInitDone && _isPickupCatalogInitDone)
+                GenerateItemLists();
+        }
+
+        private void GenerateItemLists()
+        {
+            ItemInfos.GenerateItemLists();
+            LogItems();
+
+            _configSettings.InitializeConfigFile();
+        }
+
+        private void LogItems()
+        {
+            var items = ItemInfos.GetItemInfosDictionary();
+
+            foreach (var kvp in items)
+            {
+                foreach (var item in kvp.Value)
+                    _logger.LogInfo($"{kvp.Key}:\t{item.Index} -\t{item.Tier} -\t{item.DisplayName}");
+            }
+        }
+    }
+}
---
+++ ItemRoulette/ItemInfo.cs
@@ -0,0 +1,97 @@
+﻿using RoR2;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ItemRoulette
+{
+    internal class ItemInfo
+    {
+        public ItemInfo(ItemTier itemTier, ItemIndex itemIndex, string displayName)
+        {
+            Tier = itemTier;
+            Index = itemIndex;
+            DisplayName = displayName;
+        }
+
+        public ItemTier Tier { get; private set; }
+        public ItemIndex Index { get; private set; }
+        public int IndexNumber => (int)Index;
+        public string DisplayName { get; private set; }
+
+        public bool IsNameMatch(string displayName)
+        {
+            return string.Equals(DisplayName, displayName, System.StringComparison.OrdinalIgnoreCase);
+        }
+    }
+
+    internal static class ItemInfos
+    {
+        private static IDictionary<ItemTier, ReadOnlyCollection<ItemInfo>> _itemInfosByTiers = new Dictionary<ItemTier, ReadOnlyCollection<ItemInfo>>();
+
+        public static void GenerateItemLists()
+        {
+            var itemInfosByTiers = new Dictionary<ItemTier, List<ItemInfo>>();
+
+            foreach (var itemIndex in ItemCatalog.allItems)
+            {
+                var displayName = GetPickupDef(itemIndex).nameToken;
+                if (displayName.ToLower().Contains("scrap"))
+                    continue;
+
+                var itemDef = GetItemDef(itemIndex);
+
+                if (!itemDef.inDroppableTier)
+                    continue;
+
+                var itemInfo = new ItemInfo(itemDef.tier, itemIndex, Language.GetString(displayName));
+
+                if (!itemInfosByTiers.ContainsKey(itemDef.tier))
+                    itemInfosByTiers[itemDef.tier] = new List<ItemInfo> { itemInfo };
+                else
+                    itemInfosByTiers[itemDef.tier].Add(itemInfo);
+            }
+
+            foreach (var itemInfo in itemInfosByTiers)
+                _itemInfosByTiers[itemInfo.Key] = itemInfo.Value.AsReadOnly();
+        }
+
+        public static IReadOnlyDictionary<ItemTier, ReadOnlyCollection<ItemInfo>> GetItemInfosDictionary()
+        {
+            return new ReadOnlyDictionary<ItemTier, ReadOnlyCollection<ItemInfo>>(_itemInfosByTiers);
+        }
+
+        public static IEnumerable<ItemTag> GetItemTags(ItemDef itemDef)
+        {
+            if (itemDef == null)
+                return new List<ItemTag>();
+
+            return itemDef.tags;
+        }
+
+        public static ItemDef GetItemDef(PickupIndex pickupIndex)
+        {
+            var pickupDef = GetPickupDef(pickupIndex);
+            return pickupDef == null ? null : GetItemDef(pickupDef.itemIndex);
+        }
+
+        public static PickupDef GetPickupDef(PickupIndex pickupIndex)
+        {
+            return PickupCatalog.GetPickupDef(pickupIndex);
+        }
+
+        public static PickupDef GetPickupDef(ItemIndex itemIndex)
+        {
+            return PickupCatalog.GetPickupDef(PickupCatalog.FindPickupIndex(itemIndex));
+        }
+
+        public static ItemDef GetItemDef(ItemIndex itemIndex)
+        {
+            return ItemCatalog.GetItemDef(itemIndex);
+        }
+
+        public static ItemTier GetItemTier(ItemDef itemDef)
+        {
+            return itemDef == null ? ItemTier.NoTier : itemDef.tier;
+        }
+    }
+}
